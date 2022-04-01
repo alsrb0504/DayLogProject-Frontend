6daa7c29bf710dc5022016dcdac28b0f8e87e793
MinKyu Tae <alsrb8442@naver.com>
2022-04-01 14:57:04 +0900

    ✨ make test form component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <TestLoginForm />
       </header>
     </div>
   );
 }
 
 export default App;
+
+const TestLoginForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const onError = (e) => {
+    console.error(e);
+  };
+
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <input
+          type="text"
+          {...register("username", {
+            minLength: {
+              value: 5,
+              messaage: "username must be longth than 5 characters.",
+            },
+          })}
+        />
+        <input
+          type="password"
+          {...register("password", {
+            minLength: {
+              value: 4,
+              message: "password must be longth than 4 characters.",
+            },
+          })}
+        />
+        <br />
+
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
