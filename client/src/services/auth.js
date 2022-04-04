a29f2efc3989b31371e0bfc54f08b8770cb35dec
MinKyu Tae <alsrb8442@naver.com>
2022-04-05 00:44:59 +0900

    ♻️ revise login func

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
+// axios가 request 시에는 자동으로 JSON 변환이 된다는데
+// 서버쪽에서 받아오는 res도 자동으로 JSON.parse 처리가
+// 되는 듯하다.
 export const login = (data) => {
-  const { id, password } = data;
+  // const { id, password } = data;
 
-  axios
-    .post("/api/members/login", {
-      id,
-      password,
-    })
-    .then((res) => {
-      console.log(res.data);
-    });
+  axios.post("/api/members/login", data).then((res) => {
+    console.log(res.data);
+  });
 };
