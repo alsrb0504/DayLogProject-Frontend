fd417e79ffbd520ef18366922eb5290f9aece95c
MinKyu Tae <alsrb8442@naver.com>
2022-04-04 23:33:35 +0900

    🔥 Remove TestLoginForm component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.scss";
-import axios from "axios";
-import { useForm } from "react-hook-form";
 import Login from "./pages/login";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
@@ -17,51 +15,3 @@ function App() {
 }
 
 export default App;
-
-const TestLoginForm = () => {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-
-    // GET 요청
-    // axios.get("/api").then((res) => {
-    //   console.log(res.data);
-    // });
-
-    // POST 요청
-    axios
-      .post("/api/login", {
-        id: "id",
-        passwd: "passwd",
-      })
-      .then((res) => {
-        console.log(res.data);
-      });
-  };
-
-  const onError = (e) => {
-    console.error(e);
-  };
-
-  return (
-    <>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <input type="text" {...register("username")} />
-        <input
-          type="password"
-          {...register("password", {
-            minLength: {
-              value: 4,
-              message: "password must be longth than 4 characters.",
-            },
-          })}
-        />
-        <br />
-
-        <input type="submit" value={"로그인"} />
-      </form>
-    </>
-  );
-};
