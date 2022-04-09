25209fc5aa939e534081c8462252ede0c993cb7d
MinKyu Tae <alsrb8442@naver.com>
2022-04-09 15:53:33 +0900

    🔥 remove test token code

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = (props) => {
+  const onTest = async () => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJfa…zM2fQ.GzlqwE9eDyZv4ixWhSXBtwZ5Eg1f_XvZmaKgyKvX1Ig";
+
+    const res = await axios.post("/api/members/login/token", token);
+    console.log(res);
+  };
+
   return (
     <div className="contain">
       <div className="row">
@@ -11,6 +20,8 @@ const Home = (props) => {
         <br />
         <br />
         <Link to="/signup">Move to Signup</Link>
+
+        <button onClick={onTest}>token test</button>
       </div>
     </div>
   );
