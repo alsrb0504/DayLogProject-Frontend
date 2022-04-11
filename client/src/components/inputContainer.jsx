b2057a3f3ec5875ebcdbde3553d7af7735a7c9d2
MinKyu Tae <alsrb8442@naver.com>
2022-04-11 19:51:30 +0900

    ♻️ Add label in inputContainer

--- client/src/components/inputContainer.jsx
+++ client/src/components/inputContainer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const InputContainer = ({ children, size }) => {
+const InputContainer = ({ children, size, label }) => {
   return (
-    <div className={`input-container-36 ${size ? size : "col-sm-4"}`}>
-      {children}
-    </div>
+    <>
+      {label && <label className="input-label">label</label>}
+      <div className={`input-container-36 ${size ? size : "col-sm-4"}`}>
+        {children}
+      </div>
+    </>
   );
 };
 
