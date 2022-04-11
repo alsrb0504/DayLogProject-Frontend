941001c482cc5c4ad6062ddf9fa835b1243baf06
MinKyu Tae <alsrb8442@naver.com>
2022-04-11 19:22:21 +0900

    💄 Update InputContainer size setting

--- client/src/components/inputContainer.jsx
+++ client/src/components/inputContainer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const InputContainer = ({ children }) => {
-  return <div className="input-container-36">{children}</div>;
+const InputContainer = ({ children, size }) => {
+  return (
+    <div className={`input-container-36 ${size ? size : "col-sm-4"}`}>
+      {children}
+    </div>
+  );
 };
 
 export default InputContainer;
