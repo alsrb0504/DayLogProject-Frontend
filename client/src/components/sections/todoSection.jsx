5cf7194df1d4eac4548c1267cf058eb4206fc2bd
MinKyu Tae <alsrb8442@naver.com>
2022-04-30 04:19:18 +0900

    🔨 Fix TodoSection todos prop

--- client/src/components/sections/todoSection.jsx
+++ client/src/components/sections/todoSection.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import EmptyText from "../modules/emptyText";
 import Todolist from "../modules/todolist";
 
-const TodoSection = ({ date }) => {
+const TodoSection = ({ date, todos }) => {
   return (
     <section className="todo-section">
       <h3 className="todo-section-date">{date}</h3>
 
-      <Todolist />
+      {todos && <Todolist todos={todos} />}
+      {!todos && (
+        <EmptyText text="오늘의 할 일이 없습니다.<br/>할 일을 추가해보세요." />
+      )}
     </section>
   );
 };
