8a5841df72ca5470ff33d77f8774d47faf3c3ac7
MinKyu Tae <alsrb8442@naver.com>
2022-05-04 00:10:50 +0900

    🔥 Remove not used getAuthHeader func

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -10,27 +10,16 @@ export async function login(data) {
   }
 }
 
-// 로그인 후, localStorage에 access_token 저장 함수
-// 지금은 로그인 성공 후, action 함수에서 넣어버림.
-// 나중에 구현
+// axios header에 access_token 넣는 함수
 export function SetAuthHeader() {
   const access_token = localStorage.getItem("access_token");
 
-  if (!access_token) return;
+  if (!access_token) {
+    console.err("access_token is empty in localStorage!");
+    return;
+  }
 
   axios.defaults.headers.common[
     "Authorization"
   ] = `Bearer ${localStorage.getItem("access_token")}`;
-}
-
-export function getAuthHeader() {
-  const access_token = localStorage.getItem("access_token");
-
-  if (access_token) {
-    return {
-      Authorization: `Bearer ${access_token}`,
-    };
-  } else {
-    return {};
-  }
 }
