6ce694b4843c32365e3d879f239ef2c6cac4e118
MinKyu Tae <alsrb8442@naver.com>
2022-05-05 02:15:03 +0900

    💄 Styling EmptyText Component

--- client/src/components/modules/schedulelist.jsx
+++ client/src/components/modules/schedulelist.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ScheduleItem from "./scheduleitem";
+import EmptyText from "./emptyText";
 
 const ScheduleList = ({ schedules }) => {
+  console.log(schedules);
+  console.log(typeof schedules);
+
   return (
     <ul className="schedule-container">
       {schedules &&
@@ -12,6 +16,9 @@ const ScheduleList = ({ schedules }) => {
             color={idx % 3}
           />
         ))}
+      {!schedules.length && (
+        <EmptyText text="일정이 없습니다. <br/> 일정을 추가해 보세요." />
+      )}
     </ul>
   );
 };
