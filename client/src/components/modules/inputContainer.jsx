e974366c71acc89e43fe1464e8950a5a3dbf7216
MinKyu Tae <alsrb8442@naver.com>
2022-05-05 03:19:27 +0900

    ⚡️ Add Input error bold effect and remove to print error message

--- client/src/components/modules/inputContainer.jsx
+++ client/src/components/modules/inputContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const InputContainer = ({ children, size, label }) => {
+const InputContainer = ({ children, size, label, error }) => {
   return (
     <>
       {label && <label className="input-label">{label}</label>}
-      <div className={`input-container-36 ${size ? size : "col-sm-4"}`}>
+      <div
+        className={`input-container-36 ${size ? size : "col-sm-4"} ${error}`}
+      >
         {children}
       </div>
     </>
