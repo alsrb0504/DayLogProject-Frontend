90d7d69b98efff6f9aac89f7ce0e2af295a650b4
MinKyu Tae <alsrb8442@naver.com>
2022-05-05 02:34:03 +0900

    🔥 Remove to cover div in InputContainer

--- client/src/components/modules/inputContainer.jsx
+++ client/src/components/modules/inputContainer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const InputContainer = ({ children, size, label }) => {
   return (
-    <div>
+    <>
       {label && <label className="input-label">{label}</label>}
       <div className={`input-container-36 ${size ? size : "col-sm-4"}`}>
         {children}
       </div>
-    </div>
+    </>
   );
 };
 
