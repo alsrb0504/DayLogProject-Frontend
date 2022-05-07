2fcd4ca079f83295baed802791ac264b412d7aa9
MinKyu Tae <alsrb8442@naver.com>
2022-05-07 21:03:06 +0900

    🚧 Connect SideSlideMenu and NavMenu

--- client/src/components/sections/sideSlideNavigation.jsx
+++ client/src/components/sections/sideSlideNavigation.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import MenuWhiteIcon from "../../assets/icons/menu-white.svg";
 import DefaultProfile from "../../assets/img/default-profile.svg";
+import NavMenu from "../modules/navMenu";
 
 const SideSlideNavigation = (props) => {
   // 추후 유저가 로그인하면 유저 정보를 저장하는 redux 구현
   // 이후 useSelector로 유저의 닉니엠과 프로필 이미지를 가져와서
   // 아래 메뉴들 구성
+
+  const navigate = useNavigate();
+
+  const moveHome = () => {
+    navigate("/");
+  };
+
+  const moveDiary = () => {
+    alert("구현 중");
+    return;
+    // navigate("/diary");
+  };
+
+  const moveDiet = () => {
+    alert("구현 중");
+    return;
+    // navigate('/diet');
+  };
+
+  const moveBadge = () => {
+    alert("구현 중");
+    return;
+    // navigate('/badge');
+  };
+
+  const moveBoard = () => {
+    alert("구현 중");
+    return;
+    // navigate('/board');
+  };
+
+  const moveMypage = () => {
+    alert("구현 중");
+    return;
+    // navigate('/myPage');
+  };
 
   return (
     <div>
@@ -22,21 +60,12 @@ const SideSlideNavigation = (props) => {
       </header>
       <nav>
         <ul>
-          <li>
-            <span>1</span>
-          </li>
-          <li>
-            <span>2</span>
-          </li>
-          <li>
-            <span>3</span>
-          </li>
-          <li>
-            <span>4</span>
-          </li>
-          <li>
-            <span>5</span>
-          </li>
+          <NavMenu name="홈" move={moveHome} />
+          <NavMenu name="일기" move={moveDiary} />
+          <NavMenu name="식단" move={moveDiet} />
+          <NavMenu name="뱃지 컬렉션" move={moveBadge} />
+          <NavMenu name="공유 일기 게시판" move={moveBoard} />
+          <NavMenu name="마이 페이지 및 설정" move={moveMypage} />
         </ul>
       </nav>
     </div>
