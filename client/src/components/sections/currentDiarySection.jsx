be75bc0d5300083120d6fee6d9cd3475aa36b345
MinKyu Tae <alsrb8442@naver.com>
2022-05-11 20:45:03 +0900

    ⚡️ Implement cur_diary empty case

--- client/src/components/sections/currentDiarySection.jsx
+++ client/src/components/sections/currentDiarySection.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import DiaryItem from "../modules/diaryItem";
+import EmptyText from "../modules/emptyText";
 
 const CurrentDiarySection = ({ selectedDate }) => {
   const cur_dairys = useSelector((state) => state.diary.current_diary);
+
+  const isEmpty = cur_dairys.length == 0 ? true : false;
 
   return (
     <section className="current-diary">
@@ -13,6 +16,8 @@ const CurrentDiarySection = ({ selectedDate }) => {
         {cur_dairys.map((diary, idx) => (
           <DiaryItem key={idx} idx={idx} diary={diary} />
         ))}
+
+        {isEmpty && <EmptyText text="최근에 작성한 일기가 없습니다." />}
       </ul>
     </section>
   );
