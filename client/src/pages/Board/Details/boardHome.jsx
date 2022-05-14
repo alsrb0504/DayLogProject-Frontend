5581cf804d1ff6d286768a84599ab3d96ad5db70
MinKyu Tae <alsrb8442@naver.com>
2022-05-15 04:30:26 +0900

    💄 Add boardHome header styling

--- client/src/pages/Board/Details/boardHome.jsx
+++ client/src/pages/Board/Details/boardHome.jsx
@@ -4,24 +4,29 @@ import BoardContainer from "../../../components/sections/boardContainer";
 
 const BoardHome = (props) => {
   return (
-    <>
+    <div className="board-home">
       <GlobalHeader />
       {/* 파란 부분 */}
-      <div>
-        <h2>시작이 반이다</h2>
+      <div className="board-header board-home-header">
+        <div className="board-header-bg"></div>
 
-        <nav>
-          <div>
-            <span>최신순</span>
+        <h2 className="board-header-title board-home-header-title">
+          시작이 반이다.
+        </h2>
+
+        <nav className="board-nav board-home-nav">
+          <div className="board-home-nav-left">
+            {/* 일단 최신순 선택되었다고 가정 */}
+            <span className="board-nav-active">최신순</span>
             <span>좋아요 순</span>
           </div>
 
-          <span>My Page</span>
+          <span className="board-home-nav-right board-nav-mypage">My Page</span>
         </nav>
       </div>
       {/* 목록 */}
       <BoardContainer />
-    </>
+    </div>
   );
 };
 
