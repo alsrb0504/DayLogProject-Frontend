57a68b7561d2bcf6202f477cb7aa139b8c0a35a4
MinKyu Tae <alsrb8442@naver.com>
2022-05-16 03:07:56 +0900

    ✨ Fill selected_user info to boardOther component

--- client/src/pages/Board/Details/boardOther.jsx
+++ client/src/pages/Board/Details/boardOther.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import InputHeader from "../../../components/modules/inputHeader";
 import BoardContainer from "../../../components/sections/boardContainer";
@@ -6,27 +8,41 @@ import default_profile from "../../../assets/img/default-profile.svg";
 const BoardOther = (props) => {
   const navigate = useNavigate();
 
+  const board_list = useSelector((state) => state.board.diary_list);
+  const { writer_nickname, writer_profile } = useSelector(
+    (state) => state.board.selected_user
+  );
+
+  // 새로고침 시, 리덕스 휘발성 때문에 일단 홈으로 이동.
+  useEffect(() => {
+    if (writer_nickname === "") {
+      navigate("/board");
+    }
+  }, [navigate, writer_nickname]);
+
   const moveHome = () => {
-    // 나중에 음.. 게시물로 이동하는 것으로 구현?
     navigate("/board");
   };
 
   return (
     <div className="board-other">
       <InputHeader text="공유 게시판" onClick={moveHome} />
-      {/* 파란 부분 */}
+      {/* 파란 헤더 부분 */}
       <div className="board-header board-other-header">
         <div className="board-header-bg"></div>
 
         <div className="board-other-profile-info">
           <div className="board-other-image-container">
-            <img src={default_profile} alt="프로필" />
+            <img
+              src={writer_profile ? writer_profile : default_profile}
+              alt="프로필 이미지"
+            />
           </div>
-          <span className="board-other-nickname">닉네임</span>
+          <span className="board-other-nickname">{writer_nickname}</span>
         </div>
       </div>
       {/* 목록 */}
-      {/* <BoardContainer /> */}
+      <BoardContainer diary_list={board_list} />
     </div>
   );
 };
