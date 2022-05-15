f3122630cdfe20cbb4862b6768f8b1afdb29f2a2
MinKyu Tae <alsrb8442@naver.com>
2022-05-15 15:22:22 +0900

    💄 Add board-main styling

--- client/src/components/modules/boardItem.jsx
+++ client/src/components/modules/boardItem.jsx
@@ -2,13 +2,15 @@ import dummy_image from "../../assets/img/dummy-image.png";
 
 const BoardItem = (props) => {
   return (
-    <li>
-      <div>
-        <img src={dummy_image} alt="일기 사진" />
+    <li className="board-item">
+      <div className="board-item-image-container">
+        <img className="board-item-image" src={dummy_image} alt="일기 사진" />
       </div>
 
-      <div>
-        <h3>일기 내용 ...</h3>
+      <div className="board-item-text">
+        <h3>
+          일기 내용 ... 일기 내용 ... 일기 내용 ... 일기 내용 ... 일기 내용 ...
+        </h3>
         <span>2022, May 15</span>
       </div>
     </li>
--- client/src/components/sections/boardContainer.jsx
+++ client/src/components/sections/boardContainer.jsx
@@ -1,12 +1,24 @@
-import React from "react";
 import BoardItem from "../modules/boardItem";
 
 const BoardContainer = (props) => {
   // redux에서 정보 받아오고, props로 최신순, 좋아요 순, 마이페이지 같은 것들 받아옴.
 
   return (
-    <main>
-      <ul>
+    <main className="board-main">
+      <ul className="board-container">
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
+        <BoardItem />
         <BoardItem />
         <BoardItem />
         <BoardItem />
