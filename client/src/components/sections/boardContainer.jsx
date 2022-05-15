5af05066ded07ecbaa9f5b0ef361637170062b6c
MinKyu Tae <alsrb8442@naver.com>
2022-05-15 17:27:35 +0900

    ⚡️ Add boardContainer needAdd prop

--- client/src/components/sections/boardContainer.jsx
+++ client/src/components/sections/boardContainer.jsx
@@ -1,12 +1,14 @@
 import AddBoardItem from "../modules/addBoardItem";
 import BoardItem from "../modules/boardItem";
 
-const BoardContainer = (props) => {
+const BoardContainer = ({ needAdd }) => {
   // redux에서 정보 받아오고, props로 최신순, 좋아요 순, 마이페이지 같은 것들 받아옴.
 
   return (
     <main className="board-main">
       <ul className="board-container">
+        {needAdd && <AddBoardItem />}
+
         <BoardItem />
         <BoardItem />
         <BoardItem />
