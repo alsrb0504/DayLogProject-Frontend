8e60f8bfed89a0753dfcf0d264ed079cea274ff9
MinKyu Tae <alsrb8442@naver.com>
2022-05-21 21:57:02 +0900

    ⚡️ Connect badge Reducer with badge page

--- client/src/components/modules/badgeItem.jsx
+++ client/src/components/modules/badgeItem.jsx
@@ -1,6 +1,8 @@
 import dummy_badge from "../../assets/img/dummy-badge.svg";
 
-const BadgeItem = ({ is_complete }) => {
+const BadgeItem = ({ badge }) => {
+  const { badge_no, badge_name, badge_url, is_complete } = badge;
+
   return (
     <li className="badge-item">
       <div className="badge-item-image-box">
@@ -8,11 +10,12 @@ const BadgeItem = ({ is_complete }) => {
 
         <img
           className={is_complete ? "" : "is_complete"}
-          src={dummy_badge}
+          // 추후 이미지 연결 후 수정
+          src={badge_url ? badge_url : dummy_badge}
           alt="뱃지 이미지"
         />
       </div>
-      <q className="badge-item-title">뱃지 이름</q>
+      <q className="badge-item-title">{badge_name}</q>
     </li>
   );
 };
--- client/src/pages/Badge/badgeHome.jsx
+++ client/src/pages/Badge/badgeHome.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GlobalHeader from "../../components/modules/globalHeader";
 import trophy_image from "../../assets/img/trophy.svg";
 import BadgeItem from "../../components/modules/badgeItem";
 import BadgePopup from "../../components/sections/badgePopup";
+import { useDispatch, useSelector } from "react-redux";
+import { RequestBadgeListAsync } from "../../store/actions/badge";
 
 const BadgeHome = (props) => {
+  const dispatch = useDispatch();
+
+  const badge_list = useSelector((state) => state.badge.badges);
+
+  useEffect(() => {
+    dispatch(RequestBadgeListAsync());
+  }, [dispatch]);
+
   return (
     <div className="badge">
-      <BadgePopup />
+      {/* <BadgePopup /> */}
 
       <GlobalHeader />
       <main className="badge-main">
@@ -15,24 +25,9 @@ const BadgeHome = (props) => {
           <img src={trophy_image} alt="트로피 이미지" />
         </div>
         <ul className="badge-container">
-          <BadgeItem />
-          <BadgeItem />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem />
-          <BadgeItem />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem is_complete={true} />
-          <BadgeItem />
-          <BadgeItem />
-          <BadgeItem />
-          <BadgeItem />
+          {badge_list.map((badge) => (
+            <BadgeItem key={badge.badge_no} badge={badge} />
+          ))}
         </ul>
       </main>
     </div>
