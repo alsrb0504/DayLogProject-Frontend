ffabb206490f444de7a2b037db2dbd7a82c62dd7
MinKyu Tae <alsrb8442@naver.com>
2022-05-22 14:01:48 +0900

    💄 Add choicePopup styling

--- client/src/components/modules/choiceResultPopup.jsx
+++ client/src/components/modules/choiceResultPopup.jsx
@@ -3,15 +3,20 @@ import Button from "./button";
 
 const ChoiceResultPopup = (props) => {
   return (
-    <div>
-      <h2 className="badge-popup-title">오늘의 스티커</h2>
-      <section>
-        <div>
+    <div className="choice-popup">
+      <h2 className="choice-popup-title">오늘의 스티커</h2>
+      <section className="choice-popup-main">
+        <div className="choice-image-container">
           <img src={dummy_emoji} alt="더미 이모지" />
         </div>
-        <p>스티커 관련 Text</p>
+        <p className="choice-text">스티커 관련 Text</p>
       </section>
-      <Button text="다음" color="btn-secondary" size="btn-40" />
+      <Button
+        text="다음"
+        color="btn-secondary"
+        size="btn-40"
+        className="choice-popup-btn"
+      />
     </div>
   );
 };
