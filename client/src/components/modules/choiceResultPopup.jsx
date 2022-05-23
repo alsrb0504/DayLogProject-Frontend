695365d62904704ce5c867cdda015a5299f8842b
MinKyu Tae <alsrb8442@naver.com>
2022-05-23 15:31:59 +0900

    🎨 Resize Attendance popup height

--- client/src/components/modules/choiceResultPopup.jsx
+++ client/src/components/modules/choiceResultPopup.jsx
@@ -5,19 +5,21 @@ const ChoiceResultPopup = ({ closeChoicePopup }) => {
   return (
     <div className="qa-popup choice-popup">
       <h2 className="qa-popup-title">오늘의 스티커</h2>
-      <section className="choice-popup-main">
-        <div className="choice-image-container">
-          <img src={dummy_emoji} alt="더미 이모지" />
-        </div>
-        <p className="choice-text">스티커 관련 Text</p>
-      </section>
-      <Button
-        text="다음"
-        color="btn-secondary"
-        size="btn-40"
-        className="qa-popup-btn"
-        onClick={closeChoicePopup}
-      />
+      <main className="qa-popup-main">
+        <section className="choice-popup-main">
+          <div className="choice-image-container">
+            <img src={dummy_emoji} alt="더미 이모지" />
+          </div>
+          <p className="choice-text">스티커 관련 Text</p>
+        </section>
+        <Button
+          text="다음"
+          color="btn-secondary"
+          size="btn-40"
+          className="qa-popup-btn"
+          onClick={closeChoicePopup}
+        />
+      </main>
     </div>
   );
 };
