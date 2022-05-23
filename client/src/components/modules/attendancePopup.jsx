87300ad0eb3dc81b86654491bc72b950c25d70d0
MinKyu Tae <alsrb8442@naver.com>
2022-05-23 16:16:23 +0900

    🎨 Add attendance popup background color

--- client/src/components/modules/attendancePopup.jsx
+++ client/src/components/modules/attendancePopup.jsx
@@ -12,16 +12,18 @@ const AttendancePopup = (props) => {
   return (
     <div className="qa-popup attendance-popup">
       <h2 className="qa-popup-title attendance-popup-title">이번달 출석</h2>
-      <section className="attendance-popup-main">
-        <AttendanceCalendar />
-      </section>
-      <Button
-        text="홈으로"
-        color="btn-secondary"
-        size="btn-40"
-        className="qa-popup-btn"
-        onClick={goHome}
-      />
+      <main className="qa-popup-main">
+        <section className="attendance-popup-main">
+          <AttendanceCalendar />
+        </section>
+        <Button
+          text="홈으로"
+          color="btn-secondary"
+          size="btn-40"
+          className="qa-popup-btn"
+          onClick={goHome}
+        />
+      </main>
     </div>
   );
 };
