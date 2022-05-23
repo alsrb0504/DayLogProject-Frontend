0bb91c4e8aa515171b3bb80476f74b827b6797a0
MinKyu Tae <alsrb8442@naver.com>
2022-05-23 18:29:46 +0900

    ⚡️ Make mypage confirm toggle

--- client/src/components/modules/mypageItem.jsx
+++ client/src/components/modules/mypageItem.jsx
@@ -1,6 +1,6 @@
-const MyPageItem = ({ text }) => {
+const MyPageItem = ({ text, onClick }) => {
   return (
-    <li className="mypage-menu-item">
+    <li className="mypage-menu-item" onClick={onClick}>
       <span>{text}</span>
     </li>
   );
--- client/src/pages/Mypage/mypage.jsx
+++ client/src/pages/Mypage/mypage.jsx
@@ -1,15 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import GlobalHeader from "../../components/modules/globalHeader";
 import MyPageItem from "../../components/modules/mypageItem";
+import OverLay from "../../components/modules/overLay";
+import ConfirmPopup from "../../components/modules/confirmPopup";
 
 import default_profile from "../../assets/img/default-profile.svg";
-import { useState } from "react";
 
 const MyPage = (props) => {
+  const navigate = useNavigate();
+
   const [logoutToggle, setLogoutToggle] = useState(false);
   const [resignToggle, resignLogoutToggle] = useState(false);
 
+  const openLogoutPopup = () => {
+    setLogoutToggle(true);
+  };
+
+  const closeLogoutPopup = () => {
+    setLogoutToggle(false);
+  };
+
+  const openResignPopup = () => {
+    resignLogoutToggle(true);
+  };
+
+  const closeResignPopup = () => {
+    resignLogoutToggle(false);
+  };
+
   return (
     <div className="mypage">
+      {logoutToggle && (
+        <>
+          <OverLay onClick={closeLogoutPopup} />
+          <ConfirmPopup
+            text="로그아웃 하시겠습니까?"
+            close={closeLogoutPopup}
+            // 추후 로그아웃 연결.
+            confirm={() => {}}
+          />
+        </>
+      )}
+
+      {resignToggle && (
+        <>
+          <OverLay onClick={closeResignPopup} />
+          <ConfirmPopup
+            text="회원탈퇴 하시겠습니까?"
+            close={closeResignPopup}
+            // 추후 회원탈퇴 연결.
+            confirm={() => {}}
+          />
+        </>
+      )}
+
       <GlobalHeader />
       <main className="mypage-main">
         <section className="mypage-profile">
@@ -22,8 +67,8 @@ const MyPage = (props) => {
         <ul className="mypage-menu">
           <MyPageItem text="프로필 변경" />
           <MyPageItem text="비밀 번호 변경" />
-          <MyPageItem text="로그아웃" />
-          <MyPageItem text="회원 탈퇴" />
+          <MyPageItem text="로그아웃" onClick={openLogoutPopup} />
+          <MyPageItem text="회원 탈퇴" onClick={openResignPopup} />
         </ul>
       </main>
     </div>
