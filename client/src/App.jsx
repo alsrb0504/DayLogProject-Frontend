d52966a6aa925cfe58fb4d0d1332334673bb8d05
MinKyu Tae <alsrb8442@naver.com>
2023-03-15 12:23:08 +0900

    🔥 Remote logs and Move login func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import Menstruation from "./pages/Menstruation/menstruation";
 import Schedule from "./pages/Schedule/schedule";
 import ScheduleHome from "./pages/Schedule/Details/scheduleHome";
 import ScheduleAdd from "./pages/Schedule/Details/scheduleAdd";
-import { SetAuthHeader } from "./services/auth";
+import { AutoLogin, CheckLogin, SetAuthHeader } from "./services/auth";
 import ScheduleDescription from "./pages/Schedule/Details/scheduleDescription";
 import Diary from "./pages/Diary/diary";
 import DiaryHome from "./pages/Diary/Details/diaryHome";
@@ -38,52 +38,13 @@ function App() {
 
   // req.header에 access_token 삽입.
   useEffect(() => {
-    SetAuthHeader();
-  }, []);
-
-  /*
-  useEffect(() => {
-    // 처음 접속 : 아예 access_token 이 존재 X
-    // => 로그인 페이지로 이동.
-    const cur_access_token = localStorage.getItem("access_token");
-    if (cur_access_token === null) {
-      navigate("/login");
+    if (CheckLogin()) {
+      SetAuthHeader();
+      AutoLogin(navigate);
     } else {
-      // 자동 로그인
-      axios
-        .post("/api/autoLogin", {
-          access_token: cur_access_token,
-        })
-        .then((res) => {
-          console.log(res.data);
-          const { id, name } = res.data;
-          console.log(id, name);
-        })
-        .catch((e) => {
-          console.error(e.response.status);
-          const err_message = e.response.data.message;
-
-          // Access_token이 만료된 경우
-          // refresh 토큰을 이용하여 access 토큰 재발급.
-          // 그 외의 경우는 다시 로그인
-          // 1. access 토큰이 변조됨.
-          // 2. access 토큰이 변조됨.
-          // 3. refresh 토큰 만료 or 변조.
-          if (e.response.status === 401) {
-            axios.get("/api/update").then((res) => {
-              console.log(res.data);
-              localStorage.setItem("access_token", res.data.AT);
-            });
-          } else {
-            alert(`로그인 오류 ${err_message}`);
-            navigate("/login");
-          }
-
-          console.log(e.response);
-        });
+      navigate("/login");
     }
-  }, [navigate]);
-*/
+  }, []);
 
   return (
     <div className="tablet-container">
--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -16,6 +16,36 @@ export async function login(data) {
   }
 }
 
+export async function AutoLogin(navigate) {
+  // 자동 로그인
+  axios
+    .post("/api/autoLogin")
+    .then((res) => {
+      const { id, name } = res.data;
+      console.log(id, name);
+    })
+    .catch((e) => {
+      console.error(e.response.status);
+      const err_message = e.response.data.message;
+
+      // Access_token이 만료된 경우
+      // refresh 토큰을 이용하여 access 토큰 재발급.
+      // 그 외의 경우는 다시 로그인
+      // 1. access 토큰이 변조됨.
+      // 2. access 토큰이 변조됨.
+      // 3. refresh 토큰 만료 or 변조.
+      if (e.response.status === 401) {
+        axios.get("/api/update").then((res) => {
+          console.log(res.data);
+          localStorage.setItem("access_token", res.data.AT);
+        });
+      } else {
+        alert(`로그인 오류 ${err_message}`);
+        navigate("/login");
+      }
+    });
+}
+
 // 로그인 성공 시, 받아 온 access 토큰을 local 스토리지에 저장 및
 // axios header에 넣는 함수.
 export function SetAccessToken(AT) {
